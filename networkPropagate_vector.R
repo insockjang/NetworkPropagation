f2545e01374aba2ab2a8196fa7515d50a90cba27
In Sock Jang <isjang@rcesdgvm04.rce.celgene.com>
2017-11-09 14:16:19 -0800

    add random NP to build Null model

--- networkPropagate_vector.R
+++ networkPropagate_vector.R
@@ -24,7 +24,7 @@ networkPropgate_vector<-function(f.0,alpha,Mat){
     break
   }
   
-#   f.t<-f.0
+  #   f.t<-f.0
   
   
   itcnt = 1;             
@@ -43,8 +43,60 @@ networkPropgate_vector<-function(f.0,alpha,Mat){
       print(paste("Converged at ",itcnt,sep = ""))  
       break
     }
-    itcnt = itcnt + 1;        
+    itcnt = itcnt + 1;       
+    # print(itcnt)
   }
   
   return(f.t)
 }
+
+
+networkPropgate_randomNetwork<-function(f.0,alpha,Mat,randomIter = 100){
+  # Network progate a signal on a random network
+  
+  f.0 <- f.0/sum(f.0)
+  tof = 1e-12;
+  max_iteration = 10000;
+  
+  require(foreach)
+  f.tt <- foreach(k = 1:randomIter) %dopar%{
+    set.seed(k)
+    KK <- sample(1:nrow(Mat),nrow(Mat))
+    AdjMat<-apply(Mat[KK, KK],2,function(x)colNorm(x))
+    
+    # normalized input seed matrix in colum-wise way for probability vector sum == 1
+    
+    
+    if(length(f.0) != nrow(AdjMat)){
+      print('Key missmatch error')
+      break
+    }
+    
+    #   f.t<-f.0
+    
+    itcnt = 1;             
+    
+    f.prev = f.0;    
+    while (itcnt < max_iteration){
+      
+      f.t = alpha*(AdjMat %*% f.prev) + (1-alpha)*f.0;
+      
+      
+      l1residual = norm(f.prev-f.t ,"1");
+      
+      f.prev = f.t;
+      
+      if (l1residual < tof){
+        print(paste("Converged at ",itcnt,sep = ""))  
+        break
+      }
+      itcnt = itcnt + 1;       
+      # print(itcnt)
+    }
+    
+    return(f.t)
+    
+  }
+  # normalized adjacent matrix in column-wise way
+  return(f.tt)
+  }
