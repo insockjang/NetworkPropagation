8f421045d8b5b00ed2a8eca9a7bbfe0849e12e1f
In Sock Jang <isjang@rcesdgvm04.rce.celgene.com>
2017-11-30 13:29:52 -0800

    update for mclapply

--- networkPropagate_vector.R
+++ networkPropagate_vector.R
@@ -10,7 +10,7 @@ networkPropgate_vector<-function(f.0,alpha,Mat){
   # Network progate a signal on a network
   
   # normalized adjacent matrix in column-wise way
-
+  
   
   AdjMat<-apply(Mat,2,function(x)colNorm(x))
   
@@ -155,7 +155,7 @@ networkPropgate_randomNetwork1<-function(f.0,alpha,Mat,randomIter = 100){
 }
 
 
-networkPropgate_randomNetwork2<-function(f.0,alpha,Mat,randomIter = 100){
+networkPropgate_randomNetwork2 <-function(f.0,alpha,Mat,k){
   # Network progate a signal on a random network
   
   f.0 <- f.0/sum(f.0)
@@ -163,46 +163,41 @@ networkPropgate_randomNetwork2<-function(f.0,alpha,Mat,randomIter = 100){
   max_iteration = 10000;
   
   
+  set.seed(k)
+  KK <- sample(1:nrow(Mat),nrow(Mat))
+  AdjMat<-apply(Mat[KK, KK],2,function(x)colNorm(x))
   
-  calNet<-function(k){
-    set.seed(k)
-    KK <- sample(1:nrow(Mat),nrow(Mat))
-    AdjMat<-apply(Mat[KK, KK],2,function(x)colNorm(x))
+  # normalized input seed matrix in colum-wise way for probability vector sum == 1
+  
+  
+  if(length(f.0) != nrow(AdjMat)){
+    print('Key missmatch error')
+    break
+  }
+  
+  #   f.t<-f.0
+  
+  itcnt = 1;             
+  
+  f.prev = f.0;    
+  while (itcnt < max_iteration){
     
-    # normalized input seed matrix in colum-wise way for probability vector sum == 1
+    f.t = alpha*(AdjMat %*% f.prev) + (1-alpha)*f.0;
     
     
-    if(length(f.0) != nrow(AdjMat)){
-      print('Key missmatch error')
+    l1residual = norm(f.prev-f.t ,"1");
+    
+    f.prev = f.t;
+    
+    if (l1residual < tof){
+      print(paste("Converged at ",itcnt,sep = ""))  
       break
     }
-    
-    #   f.t<-f.0
-    
-    itcnt = 1;             
-    
-    f.prev = f.0;    
-    while (itcnt < max_iteration){
-      
-      f.t = alpha*(AdjMat %*% f.prev) + (1-alpha)*f.0;
-      
-      
-      l1residual = norm(f.prev-f.t ,"1");
-      
-      f.prev = f.t;
-      
-      if (l1residual < tof){
-        print(paste("Converged at ",itcnt,sep = ""))  
-        break
-      }
-      itcnt = itcnt + 1;       
-      # print(itcnt)
-    }
-    return(f.t)
+    itcnt = itcnt + 1;       
+    # print(itcnt)
   }
-  f.tt <- mclapply(1:randomIter, function(x){calNet(x)}, mc.core = 32)
-  # normalized adjacent matrix in column-wise way
-  return(f.tt)
+  return(f.t)
+  
 }
 
 
