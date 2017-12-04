ce2a9751fa6c56058f1db4c410efbc6c178d1fab
In Sock Jang <isjang@rcesdgvm04.rce.celgene.com>
2017-12-04 14:21:36 -0800

    randomSeed update

--- networkPropagate_vector.R
+++ networkPropagate_vector.R
@@ -213,7 +213,7 @@ networkPropgate_randomSeed<-function(f.0,alpha,Mat,k){
   set.seed(k)
   f.0 <- sample(f.0)
   # normalized input seed matrix in colum-wise way for probability vector sum == 1
-
+  
   if(length(f.0) != nrow(AdjMat)){
     print('Key missmatch error')
     break
@@ -242,4 +242,54 @@ networkPropgate_randomSeed<-function(f.0,alpha,Mat,k){
   }
   return(f.t)
   
+}
+
+
+networkPropgate_randomSeed1<-function(f.0,alpha,Mat,randomIter = 100){
+  # Network progate a signal on a random network
+  
+  f.0 <- f.0/sum(f.0)
+  tof = 1e-12;
+  max_iteration = 10000;
+  AdjMat<-apply(Mat,2,function(x)colNorm(x))
+  
+  f.tt <- vector("list",randomIter)
+  
+  for(k in 1:randomIter){
+    set.seed(k)
+    f.0 <- sample(f.0)
+    # normalized input seed matrix in colum-wise way for probability vector sum == 1
+    
+    if(length(f.0) != nrow(AdjMat)){
+      print('Key missmatch error')
+      break
+    }
+    
+    #   f.t<-f.0
+    
+    itcnt = 1;             
+    
+    f.prev = f.0;    
+    while (itcnt < max_iteration){
+      
+      f.t = alpha*(AdjMat %*% f.prev) + (1-alpha)*f.0;
+      
+      
+      l1residual = norm(f.prev-f.t ,"1");
+      
+      f.prev = f.t;
+      
+      if (l1residual < tof){
+        print(paste("Converged at ",itcnt,sep = ""))  
+        break
+      }
+      itcnt = itcnt + 1;       
+      # print(itcnt)
+    }
+    f.tt[[k]] <- f.t
+  }
+  
+  # normalized adjacent matrix in column-wise way
+  return(f.tt)
+
 }
