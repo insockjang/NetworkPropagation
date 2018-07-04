cbfa6bbe76a73788def57feffff8f8d1a7825589
In Sock Jang <isjang@rcesdgvm04.rce.celgene.com>
2018-07-03 20:56:23 -0700

    update parameter

--- networkPropagate_vector.R
+++ networkPropagate_vector.R
@@ -19,7 +19,7 @@ networkPropgate_vector<-function(f.0,alpha,Mat){
   f.0 <- f.0/sum(f.0)
   
   tof = 1e-12;
-  max_iteration = 10000;
+  max_iteration = 1000000;
   
   if(length(f.0) != nrow(AdjMat)){
     print('Key missmatch error')
